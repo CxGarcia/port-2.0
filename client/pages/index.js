148a46bad340bf8e7ec889f6e085ddb1c187b1c5
cxgarcia <schlaubitzcristobal@gmail.com>
2021-04-16 14:12:32 +0200

    feat: social media icons and cv download

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,32 +1,77 @@
 import Logo from '@resources/logos/cx';
 import Mountains from '@components/home/Mountains';
 
+import LinkedIn from '@resources/icons/social/linkedin.svg';
+import GitHub from '@resources/icons/social/github.svg';
+import CV from '@resources/icons/social/cv.svg';
+import TransitionMountain from '@resources/others/section-transition-mountain.svg';
+
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <nav className={styles.nav}>
-        <Logo className={styles.logo} />
+        <h1 className={styles.logo}>
+          cxgarcia<span>_</span>
+        </h1>
+        <div className={styles.social}>
+          <a
+            href="https://www.linkedin.com/in/schlaubitzcristobal/"
+            target="_blank"
+            aria-label="Link to LinkedIn profile"
+          >
+            <LinkedIn />
+          </a>
+          <a
+            href="https://github.com/cxgarcia"
+            target="_blank"
+            aria-label="Link to GitHub profile"
+          >
+            <GitHub />
+          </a>
+          <a
+            href="personal-resources/cv-cristobal-schlaubitz-garcia.pdf"
+            download
+          >
+            <CV />
+          </a>
+        </div>
       </nav>
 
       <section className={`${styles.about} about`}>
-        <div className={styles.aboutText}>
-          <h1>Cristobal Schlaubitz Garcia</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+        <div className={styles.sectionText}>
+          <h2>
+            Hey There! My name is Cristobal and I am a Full Stack Developer from
+            Venezuela
+          </h2>
+          <p></p>
         </div>
         <Mountains />
       </section>
-      <section className={styles.synthesis}></section>
-      <section className={styles.plantopi}></section>
+      <section className={styles.synthesis}>
+        <h1>Synthesis</h1>
+        <p>
+          Synthesis is an open-source Digital Audio Workstation built in React
+          and Koa.js so that music producers can make beats from the comfort of
+          their browser. The app's library is furnished with thousands of
+          samples, various synthesizers, and digital effects to make music from
+          a wide range of genres.
+        </p>
+      </section>
+      <section className={styles.plantopi}>
+        <TransitionMountain className={styles.transitionMountain} />
+        <h1>Plantopi</h1>
+        <p>
+          Plantopi is an automatic irrigation system that allows plant owners to
+          remotely water and monitor their plants. Users can create multiple
+          areas, each with their own set of humidity sensors and controllers
+          (water pumps, electrovalves, lights, etc.), and setup irrigation
+          schedules based on their plants' needs. The application communicates
+          with Arduinos and a Raspberry Pi for IoT control and makes use of Open
+          Weather API for weather data.
+        </p>
+      </section>
 
       <footer className={styles.footer}></footer>
     </div>
